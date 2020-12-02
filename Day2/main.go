711914f90a95c0c49aa51f78fcfdb78f8d50b0dc
Joel Ardarve <joel.ardarve@gmail.com>
2020-12-02 08:02:54 +0100

    Dat 2 part 2 complete

--- Day2/main.go
+++ Day2/main.go
@@ -15,7 +15,7 @@ func check(e error){
   }
 }
 
-func checkValidPassword(lowerLimit int, upperLimit int, character string, password string) bool{
+func checkValidPasswordPart1(lowerLimit int, upperLimit int, character string, password string) bool {
   count := strings.Count(password, character)
   if(count >= lowerLimit && count <= upperLimit){
     return true
@@ -24,22 +24,37 @@ func checkValidPassword(lowerLimit int, upperLimit int, character string, passwo
   }
 }
 
-func checkValidData(text []string) int{
-  var validPasswords = 0
+func checkValidPasswordPart2(lowerIndex int, upperIndex int , character string, password string) bool {
+  a := string(password[lowerIndex-1])
+  b := string(password[upperIndex-1])
+  if((a == character) !=  (b == character)){
+    return true
+  }
+  return false
+}
+
+func parseDataAndCheckPassword(text []string) (int, int){
+  var validPasswordsPart1 = 0
+  var validPasswordsPart2 = 0
   for _, line := range text {
     data := strings.Split(line, " ")
     limits := strings.Split(data[0], "-")     //limits[0] = lower limit, limits[1] = upper limit
-    lowerLimit, _ := strconv.Atoi(limits[0])
-    upperLimit, _ := strconv.Atoi(limits[1])
-    char := strings.Split(data[1], ":")[0] 
+    lowerLimit, _ := strconv.Atoi(limits[0])  //Lower limit
+    upperLimit, _ := strconv.Atoi(limits[1])  //Upper limit
+    char := strings.Split(data[1], ":")[0]    //Character
     password := data[2]                       //Password
-    if (checkValidPassword(lowerLimit, upperLimit, char, password)){
-      validPasswords = validPasswords + 1
+
+    if (checkValidPasswordPart1(lowerLimit, upperLimit, char, password)){
+      validPasswordsPart1 = validPasswordsPart1 + 1
+    }
+    if (checkValidPasswordPart2(lowerLimit, upperLimit, char, password)){
+      validPasswordsPart2 = validPasswordsPart2 + 1
     }
   }
-  fmt.Printf("part 1: %v\n", validPasswords)
-  return validPasswords
+  fmt.Printf("Part 1: %v\nPart 2: %v\n", validPasswordsPart1, validPasswordsPart2)
+  return validPasswordsPart1, validPasswordsPart2
 }
+
 func readFile(input string) (text []string){
   file, err := os.Open(input)
   check(err)
@@ -54,5 +69,5 @@ func readFile(input string) (text []string){
 
 func main(){
   var data = readFile(input)
-  checkValidData(data)
+  parseDataAndCheckPassword(data)
 }
