6d1e790a1fb3046e4389c86a4d832d8809fcea47
Joel Ardarve <joel.ardarve@gmail.com>
2020-12-06 17:32:46 +0100

    Day 6 part 2 complete

--- Day6/main.go
+++ Day6/main.go
@@ -11,6 +11,7 @@ import (
 
 var input = "input"
 type IntSet map[string]bool
+type RealIntSet map[string]int
 
 func check(e error)bool{
   if e != nil {
@@ -38,6 +39,42 @@ func createSetListOfData(data []string)[]IntSet{
   return listOfSets
 }
 
+func createSetListOfDataPart2(data []string)[]RealIntSet{
+  listOfSets := []RealIntSet{}
+  set := RealIntSet{}
+  var numberOfPeople = 0
+  for _, line := range data {
+    if line == "" {
+      set["size"] = numberOfPeople
+      numberOfPeople = 0
+      listOfSets = append(listOfSets, set)
+      set = RealIntSet{}
+    }else {
+      numberOfPeople = numberOfPeople + 1
+      for _, char := range line {
+        set[string(char)] = set[string(char)] + 1
+      }
+    }
+  }
+  set["size"] = numberOfPeople
+  listOfSets = append(listOfSets, set)
+  return listOfSets
+}
+
+func calculateSumOfVotesInSetsPart2(setData []RealIntSet) int {
+  var amountOfVotes = 0
+  for _, set := range setData{
+    for key, setAmount := range set {
+      if(key != "size"){
+        if set["size"] == setAmount {
+          amountOfVotes = amountOfVotes + 1
+        }
+      }
+    }
+  }
+  return amountOfVotes
+}
+
 func calculateSumOfVotesInSets(setData []IntSet) int {
   var amountOfVotes = 0
   for _, set := range setData {
@@ -60,7 +97,10 @@ func readFile(input string) (text []string){
 
 func main(){
   var data = readFile(input)
-  var dataSet = createSetListOfData(data)
-  var part1Result = calculateSumOfVotesInSets(dataSet)
+  var dataSetPart1 = createSetListOfData(data)
+  var dataSetPart2 = createSetListOfDataPart2(data)
+  var part1Result = calculateSumOfVotesInSets(dataSetPart1)
+  var part2Result = calculateSumOfVotesInSetsPart2(dataSetPart2)
   fmt.Printf("Part 1: %v\n", part1Result)
+  fmt.Printf("Part 2: %v\n", part2Result)
 }
