79b9745d2ac1c4dd715bd3306532912c726f4513
Joel Ardarve <joel.ardarve@gmail.com>
2020-12-09 21:57:49 +0100

    Finished Day 8 part 2

--- Day8/main.go
+++ Day8/main.go
@@ -64,15 +64,18 @@ func RunProgramUntilRepeatedInstruction(program *Program)int{
 	}
 }
 
-func RunUntilProgramOutOfBounds(program *Program)int{
-	for{
-		ParseAndExecuteInstruction(program)
-		if program.OutOfBounds{
-			return program.Accumilator
-		}else if program.Infinite {
-			return 0
+func RunUntilProgramOutOfBounds(programs *[]Program)int{
+	for _, program := range *programs{
+		for{
+			ParseAndExecuteInstruction(&program)
+			if program.OutOfBounds{
+				return program.Accumilator
+			}else if program.Infinite || program.RepeatedInstruction{
+				break
+			}
 		}
 	}
+	return 0
 }
 
 func ParseAndExecuteInstruction(program *Program){
@@ -83,7 +86,6 @@ func ParseAndExecuteInstruction(program *Program){
 
 	CheckIfOutOfBounds = func(){
 		if program.Pointer > len(program.Instructions)-1{
-			fmt.Println("Program out of bounds")
 			program.OutOfBounds = true
 		}
 	}
@@ -98,7 +100,6 @@ func ParseAndExecuteInstruction(program *Program){
 		for _, val := range program.PrevInstructions{
 			if val == program.Pointer{
 				program.RepeatedInstruction = true
-				fmt.Println("Repeated instruction")
 			}
 		}
 	}
@@ -116,7 +117,6 @@ func ParseAndExecuteInstruction(program *Program){
 	var instruction = strings.Split(program.Instructions[program.Pointer], " ")
 	var op = instruction[0]
 	var argument, _ = strconv.Atoi(instruction[1])
-	//fmt.Printf("op: %v, arg: %v, Pointer: %v\n", op, argument, program.Pointer)
 	switch op{
 		case "acc":
 		program.Accumilator = program.Accumilator + argument
@@ -134,7 +134,43 @@ func ParseAndExecuteInstruction(program *Program){
 }
 
 func BuildProgramPermutations(program *Program)*[]Program{
+	//Deep copy
+	var CopyProgram = func(program Program)Program{
+		newProgram := program
+		newProgram.Instructions = make([]string, len(program.Instructions))
+		newProgram.PrevInstructions = make([]int, len(program.PrevInstructions))
+		copy(newProgram.Instructions, program.Instructions)
+		copy(newProgram.PrevInstructions, program.PrevInstructions)
+		return newProgram
+	}
+
 	programList := []Program{}
+	var numberOfNops = 0
+	var numberOfJmps = 0
+	var nopIndex []int
+	var jmpIndex []int
+
+	//Build 2 lists of jumps and nops
+	for index, val := range program.Instructions{
+		if strings.Contains(val, "nop"){
+			numberOfNops = numberOfNops + 1
+			nopIndex = append(nopIndex, index)
+		}else if strings.Contains(val, "jmp"){
+			numberOfJmps = numberOfJmps + 1
+			jmpIndex = append(jmpIndex, index)
+		}
+	}
+
+	for _, index := range nopIndex {
+		newProgram := CopyProgram(*program)
+		newProgram.Instructions[index] = strings.Replace(newProgram.Instructions[index], "nop", "jmp", 1)
+		programList = append(programList, newProgram)
+	}
+	for _, index := range jmpIndex {
+		newProgram := CopyProgram(*program)
+		newProgram.Instructions[index] = strings.Replace(newProgram.Instructions[index], "jmp", "nop", 1)
+		programList = append(programList, newProgram)
+	}
 	return &programList
 }
 
@@ -142,8 +178,10 @@ func main(){
 	var data = ReadFile(input)
 	var program = LoadProgram(data)
 	var part1 = RunProgramUntilRepeatedInstruction(program)
-	fmt.Printf("Part 1: %v", part1)
+	fmt.Printf("Part 1: %v\n", part1)
 
 	program = LoadProgram(data)
-	BuildProgramPermutations(program)
+	var allThePrograms = BuildProgramPermutations(program)
+	var part2 = RunUntilProgramOutOfBounds(allThePrograms)
+	fmt.Printf("Part 2: %v", part2)
 }
