40440660288991a8a65711538a3be5b791e00d3b
Joel Ardarve <joel.ardarve@gmail.com>
2020-12-09 09:40:35 +0100

    Overengineered part 1

--- Day8/main.go
+++ Day8/main.go
@@ -15,7 +15,10 @@ type Program struct {
 	Accumilator int
 	Instructions []string
 	PrevInstructions []int
-	Terminate bool
+	RepeatedInstruction bool
+	Counter int
+	Infinite bool
+	OutOfBounds bool
 }
 
 func ReadFile(input string) (text []string){
@@ -41,7 +44,10 @@ func LoadProgram(data []string) *Program {
 	program := Program{}
 	program.Pointer = 0
 	program.Accumilator = 0
-	program.Terminate = false
+	program.Counter = 0
+	program.RepeatedInstruction = false
+	program.Infinite = false
+	program.OutOfBounds = false
 
 	for _, line := range data {
 		program.Instructions = append(program.Instructions, line)
@@ -49,54 +55,95 @@ func LoadProgram(data []string) *Program {
 	return &program
 }
 
-func RunProgramUntilRepeatedInstruction(program *Program){
+func RunProgramUntilRepeatedInstruction(program *Program)int{
 	for{
 		ParseAndExecuteInstruction(program)
-		if(program.Terminate){
-			return
+		if(program.RepeatedInstruction){
+			return program.Accumilator
+		}
+	}
+}
+
+func RunUntilProgramOutOfBounds(program *Program)int{
+	for{
+		ParseAndExecuteInstruction(program)
+		if program.OutOfBounds{
+			return program.Accumilator
+		}else if program.Infinite {
+			return 0
 		}
 	}
 }
 
 func ParseAndExecuteInstruction(program *Program){
 	var StepProgram func(steps int)
-	var CheckIfTerminate func()bool
-	CheckIfTerminate = func()bool{
+	var CheckIfRepeatedInstruction func()
+	var CheckIfInfinite func()
+	var CheckIfOutOfBounds func()
+
+	CheckIfOutOfBounds = func(){
+		if program.Pointer > len(program.Instructions)-1{
+			fmt.Println("Program out of bounds")
+			program.OutOfBounds = true
+		}
+	}
+
+	CheckIfInfinite = func(){
+		if(program.Counter > 100000){
+			program.Infinite = true
+		}
+	}
+
+	CheckIfRepeatedInstruction = func(){
 		for _, val := range program.PrevInstructions{
 			if val == program.Pointer{
-				program.Terminate = true
-				return true
+				program.RepeatedInstruction = true
+				fmt.Println("Repeated instruction")
 			}
 		}
-		return false
 	}
+
 	StepProgram = func(steps int){
+		program.Counter = program.Counter + 1
 		program.PrevInstructions = append(program.PrevInstructions, program.Pointer)
 		program.Pointer = program.Pointer + steps
-		if CheckIfTerminate(){
-			return
-		}
+
+		CheckIfInfinite()
+		CheckIfRepeatedInstruction()
+		CheckIfOutOfBounds()
 	}
+
 	var instruction = strings.Split(program.Instructions[program.Pointer], " ")
 	var op = instruction[0]
 	var argument, _ = strconv.Atoi(instruction[1])
+	//fmt.Printf("op: %v, arg: %v, Pointer: %v\n", op, argument, program.Pointer)
 	switch op{
 		case "acc":
 		program.Accumilator = program.Accumilator + argument
 		StepProgram(1)
 		break
+
 		case "jmp":
 		StepProgram(argument)
 		break
+
 		case "nop":
 		StepProgram(1)
 		break
 	}
 }
 
+func BuildProgramPermutations(program *Program)*[]Program{
+	programList := []Program{}
+	return &programList
+}
+
 func main(){
 	var data = ReadFile(input)
 	var program = LoadProgram(data)
-	RunProgramUntilRepeatedInstruction(program)
-	fmt.Printf("Part 1: %v", program.Accumilator)
+	var part1 = RunProgramUntilRepeatedInstruction(program)
+	fmt.Printf("Part 1: %v", part1)
+
+	program = LoadProgram(data)
+	BuildProgramPermutations(program)
 }
